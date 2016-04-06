466d7b0d9f963fb6329059742e4b3fb9cb7607ad
Clint Armstrong <clint@clintarmstrong.net>
2016-04-06 10:12:30 -0400

    fix transpose cracker

--- pw-concurrent.py
+++ pw-concurrent.py
@@ -1,6 +1,7 @@
 from electrum import bitcoin
 import itertools
-import concurrent.futures
+from Queue import Queue
+from threading import Thread
 
 u = [
      ["y", "Y"],
@@ -8,13 +9,16 @@ u = [
      ["u", "U"]
     ]
 
+
+pw_prefix = [
+       ["", "?", "^", "*", "&", "!", "|"],
+       ["", "7980", "8990", "5950", "1010220"],
+       ["", "7980", "8990", "5950", "1010220"],
+    ]
+
 u = ["".join(i) for i in itertools.product(*u)]
 
 pw = [
-       ["", "7980", "8990", "5950", "1010220"],
-       ["", "?", "^", "*", "&", "!", "|"],
-       ["", "7980", "8990", "5950", "1010220"],
-       ["", "?", "^", "*", "&", "!", "|"],
        ["e", "E", "3", "=", "[-"],
        ["l", "L", "|", "1", "!", "|_", "["],
        ["e", "E", "3", "=", "[-"],
@@ -28,27 +32,34 @@ pw = [
 
 s = "STcRhPfZeossbG60ZksnkuPVOicUb5dmOKiTS7Ch02AAJvXvGX0hu+qNZFaBbpGk/Jz6y6fCW54sjYKqZEFJs+D3ZUQNMPTj87CN3uC47z+pzhtDJt5ssfHkoJrL1KPwSWDxx0YnMUDg3b5gpIQnyNo60HBmT2c+ICOmryxQOio="
 
-def grouper(iterable, n, fillvalue=None):
-    "Collect data into fixed-length chunks or blocks"
-    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
-    args = [iter(iterable)] * n
-    return itertools.izip_longest(*args, fillvalue=fillvalue)
+q = Queue(20)
 
-def try_item(item):
-  p = "".join(item)
-  #print p
-  try:
-    bitcoin.pw_decode(p, i)
-    print p
-    print "success"
-  except:
-    pass
+def worker():
+  while True:
+    prefix = q.get()
+    for i in itertools.product(*pw):
+      p = str(prefix+"".join(i))
+      #print p
+      try:
+        bitcoin.pw_decode(s, p)
+        solved = True
+        print p
+        print "success"
+        with open('password.txt', 'w') as f:
+          f.write(p)
+      except:
+        pass
+    q.task_done()
 
-def try_group(pws):
-  for i in pws:
-    try_item(i)
+for i in range(6):
+  t = Thread(target=worker)
+  t.daemon = True
+  t.start()
+    
+for i in itertools.product(*pw_prefix):
+  #print q.qsize()
+  q.put(str(i[1]+i[0]+i[2]+i[0]))
 
-with concurrent.futures.ThreadPoolExecutor() as executor:
-     executor.map(try_item, itertools.product(*pw))
+q.join()
 
 print "done"
--- smart-cracker-test.py
+++ smart-cracker-test.py
@@ -1,15 +1,14 @@
 import itertools
 import electrum
 
-words = u'cargo series gloom wing normal velvet view sock wing rib fat acid'
-pub = "xpub661MyMwAqRbcFdEFzAGcVaokv2MXeRTFRjZVUu5eNusyYYkcmV4HWBCjuodH82WhmRytEyHLQSRsNSuNo8DjtzDoYk69o7pfHwi9N5gTqa7"
-
+words = u"tribe lady provide usual disagree small end hurt place genre neutral able wagon"
+pub = "xpub661MyMwAqRbcFMdvbfzf2JcUvVZPxa1eqWNKwgp9QkNNJvj5tr6Gt86FkvpL4ooUvyMrEHY2fUusdULFxpp9ej46aESgWQzJmEB8xdi2Fj1"
 
 i = 0
-z = len(words.split()) - 1
-while i < z:
+while i < 12:
   s = electrum.WalletStorage('/home/clint/.electrum/wallets/smart-cr-tst')
   arr = words.split()
+  print len(arr)
   arr[i], arr[i+1] = arr[i+1], arr[i]
   seed = " ".join(arr)
   try:
