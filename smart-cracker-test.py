8378a9be70feaa82d0d25586834ef4c66edb0269
Clint Armstrong <clint@clintarmstrong.net>
2016-04-07 08:59:37 -0400

    working solution!

--- smart-cracker-test.py
+++ smart-cracker-test.py
@@ -1,22 +1,34 @@
 import itertools
 import electrum
+import unicodedata
 
-words = u"tribe lady provide usual disagree small end hurt place genre neutral able wagon"
+words = u"tribe lady provide usual disagree small end hurt place genre neutral wagon"
 pub = "xpub661MyMwAqRbcFMdvbfzf2JcUvVZPxa1eqWNKwgp9QkNNJvj5tr6Gt86FkvpL4ooUvyMrEHY2fUusdULFxpp9ej46aESgWQzJmEB8xdi2Fj1"
 
+s = open('/home/clint/src/github.com/spesmilo/electrum/lib/wordlist/english.txt').read().strip()
+s = unicodedata.normalize('NFKD', s.decode('utf8'))
+lines = s.split('\n')
+wordlist = []
+for line in lines:
+    line = line.split('#')[0]
+    line = line.strip(' \r')
+    assert ' ' not in line
+    if line:
+        wordlist.append(line)
+
 i = 0
-while i < 12:
+z = len(words.split())
+while i <= z:
   s = electrum.WalletStorage('/home/clint/.electrum/wallets/smart-cr-tst')
   arr = words.split()
-  print len(arr)
-  arr[i], arr[i+1] = arr[i+1], arr[i]
-  seed = " ".join(arr)
-  try:
-    w = electrum.Wallet.from_text(seed, None, s)
+  for w in wordlist:
+    seed = " ".join(arr[:i] + [w] + arr[i:])
     print seed
-    if w.get_master_public_key() == pub:
-      print 'success'
-      break
-  except BaseException:
-    pass
+    try:
+      w = electrum.Wallet.from_text(seed, None, s)
+      if w.get_master_public_key() == pub:
+        print 'success'
+        break
+    except BaseException:
+      pass
   i += 1
--- smart-cracker.py
+++ smart-cracker.py
@@ -1,23 +1,33 @@
 import itertools
 import electrum
+import unicodedata
 
 words = u'cargo series gloom wing normal velvet view sock wing rib fat acid'
+#words = u'cargo series gloom wing normal velvet view sock rib fat acid'
 pub = "xpub661MyMwAqRbcFdEFzAGcVaokv2MXeRTFRjZVUu5eNusyYYkcmV4HWBCjuodH82WhmRytEyHLQSRsNSuNo8DjtzDoYk69o7pfHwi9N5gTqa7"
 
+s = open('/home/clint/src/github.com/spesmilo/electrum/lib/wordlist/english.txt').read().strip()
+s = unicodedata.normalize('NFKD', s.decode('utf8'))
+lines = s.split('\n')
+wordlist = []
+for line in lines:
+    line = line.split('#')[0]
+    line = line.strip(' \r')
+    assert ' ' not in line
+    if line:
+        wordlist.append(line)
 
-i = 0
-z = len(words.split()) - 1
-while i < z:
-  s = electrum.WalletStorage('/home/clint/.electrum/wallets/smart-cr-tst')
-  arr = words.split()
-  arr[i], arr[i+1] = arr[i+1], arr[i]
-  seed = " ".join(arr)
-  try:
-    w = electrum.Wallet.from_text(seed, None, s)
-    print seed
-    if w.get_master_public_key() == pub:
-      print 'success'
-      break
-  except BaseException:
-    pass
-  i += 1
+for word in wordlist:
+  for i in range(0, len(words.split())):
+    s = electrum.WalletStorage('/home/clint/.electrum/wallets/smart-cr-tst')
+    arr = words.split()
+    seed = " ".join(arr[:i] + [word] + arr[i:])
+    try:
+      w = electrum.Wallet.from_text(seed, None, s)
+      if w.get_master_public_key() == pub:
+        print seed
+        print 'success'
+        break
+    except BaseException:
+      pass
+    i += 1
