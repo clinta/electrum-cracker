c3a4ecabbab5b176cb40d441d135a9e85ddd91fe
Clint Armstrong <clint@clintarmstrong.net>
2016-04-07 09:00:55 -0400

    clean up

--- cracker.py
+++ cracker.py
@@ -1,56 +1,32 @@
 import itertools
-from datetime import datetime
-from datetime import timedelta
-from electrum import bitcoin
-from unicodedata import normalize
+import electrum
+import unicodedata
 
-words = u"cargo series gloom wing normal velvet sock acid fat view wing rib"
+words = u'cargo series gloom wing normal velvet view sock wing rib fat acid'
 pub = "xpub661MyMwAqRbcFdEFzAGcVaokv2MXeRTFRjZVUu5eNusyYYkcmV4HWBCjuodH82WhmRytEyHLQSRsNSuNo8DjtzDoYk69o7pfHwi9N5gTqa7"
 
+s = open('/home/clint/src/github.com/spesmilo/electrum/lib/wordlist/english.txt').read().strip()
+s = unicodedata.normalize('NFKD', s.decode('utf8'))
+lines = s.split('\n')
+wordlist = []
+for line in lines:
+    line = line.split('#')[0]
+    line = line.strip(' \r')
+    assert ' ' not in line
+    if line:
+        wordlist.append(line)
 
-root_derivation = "m/"
-
-def normalize_passphrase(passphrase):
-    return normalize('NFKD', unicode(passphrase or ''))
-
-def mnemonic_to_seed(mnemonic, passphrase):
-    # See BIP39
-    import pbkdf2, hashlib, hmac
-    PBKDF2_ROUNDS = 2048
-    mnemonic = normalize('NFKD', ' '.join(mnemonic.split()))
-    passphrase = normalize_passphrase(passphrase)
-    return pbkdf2.PBKDF2(mnemonic, 'mnemonic' + passphrase,
-                         iterations = PBKDF2_ROUNDS, macmodule = hmac,
-                         digestmodule = hashlib.sha512).read(64)
-
-def xpub_from_seed(seed):
-    # store only master xpub
-    xprv, xpub = bitcoin.bip32_root(mnemonic_to_seed(seed,''))
-    xprv, xpub = bitcoin.bip32_private_derivation(xprv, "m/", root_derivation)
-    return xpub
-
-def xprv_from_seed(seed):
-    # we don't store the seed, only the master xpriv
-    xprv, xpub = bitcoin.bip32_root(mnemonic_to_seed(seed, ''))
-    xprv, xpub = bitcoin.bip32_private_derivation(xprv, "m/", root_derivation)
-    return xprv
-
-i = 0
-total = 479001600
-print total
-start = datetime.now()
-for subset in itertools.permutations(words.split()):
-  i+=1
-  seed = " ".join(subset)
-  if i % 2000 == 0:
-    elapsed = datetime.now() - start
-    rate = i / elapsed.seconds
-    remaining = total - i
-    finish = datetime.now() + timedelta(seconds=remaining/rate)
-    print i,"/",total,"  ",float(i/total),"%  finish: ",finish
-  if bitcoin.is_new_seed(seed):
-    #print xpub_from_seed(seed)
-    if str(xpub_from_seed(seed)) == str(pub):
-      print(seed)
-      print(xprv_from_seed(seed))
-      break
+for word in wordlist:
+  for i in range(0, len(words.split())):
+    s = electrum.WalletStorage('/home/clint/.electrum/wallets/smart-cr-tst')
+    arr = words.split()
+    seed = " ".join(arr[:i] + [word] + arr[i:])
+    try:
+      w = electrum.Wallet.from_text(seed, None, s)
+      if w.get_master_public_key() == pub:
+        print seed
+        print 'success'
+        break
+    except BaseException:
+      pass
+    i += 1
--- permutations.py
+++
@@ -1,6 +0,0 @@
-import itertools
-
-words = u"cargo series gloom wing normal velvet sock acid fat view wing rib"
-
-for subset in itertools.permutations(words.split()):
-  print " ".join(subset)
